78044a65107cc518ed7930330d51136f0acf3f67
Nicholas Pung <nckpng95@gmail.com>
2019-10-31 15:16:46 -0700

    Cleaned up int/string errors and also made transactions work

--- basic_wallet_p/wallet/src/App.js
+++ basic_wallet_p/wallet/src/App.js
@@ -26,6 +26,9 @@ function App() {
   const [coins, setCoins] = useState("");
   const [startTime, setStartTime] = useState(Date.now());
   const [lastUpdated, setLastUpdated] = useState(0);
+  const [recipient, setRecipient] = useState("");
+  const [transaction, setTransaction] = useState(0);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/chain", {
@@ -54,8 +57,21 @@ function App() {
     setId(event.target.value);
   };
 
+  const onRecipientChange = event => {
+    setRecipient(event.target.value);
+  };
+
+  const onTransactionChange = event => {
+    setTransaction(event.target.value);
+  };
+
   const onIdSubmit = event => {
     event.preventDefault();
+    let amount = checkCoins();
+    setCoins(`${parseInt(amount)}`);
+  };
+
+  const checkCoins = () => {
     let idTransactions = [];
     let amount = 0;
     blockchain.chain.map(block => {
@@ -64,16 +80,48 @@ function App() {
       );
       idTransactions = idTransactions.concat(filtered);
     });
+    console.log(idTransactions);
     idTransactions.map(transaction => {
       if (transaction.sender === "0") {
         amount += 1;
       } else if (transaction.sender === id) {
-        amount -= transaction.amount;
+        amount -= parseInt(transaction.amount);
       } else if (transaction.recipient === id) {
-        amount += transaction.amount;
+        amount += parseInt(transaction.amount);
       }
     });
-    setCoins(amount);
+    console.log(amount);
+
+    return amount;
+  };
+
+  const onTransactionSubmit = event => {
+    event.preventDefault();
+    let amount = checkCoins();
+    if (parseInt(amount) < parseInt(transaction)) {
+      setMessage("Not enough coins to transfer.");
+      return;
+    }
+    let data = JSON.stringify({
+      sender: id,
+      recipient: recipient,
+      amount: transaction
+    });
+    fetch("http://localhost:5000/transactions/new", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      mode: "cors",
+      method: "POST",
+      body: data
+    })
+      .then(response => response.json())
+      .then(response => {
+        setMessage(response.message);
+        setTransaction(0);
+        setRecipient("");
+      });
   };
 
   return (
@@ -87,6 +135,7 @@ function App() {
           name="id"
           value={id}
           onChange={event => onIdChange(event)}
+          placeholder="ID..."
         />
         <button onClick={event => onIdSubmit(event)}>Check Wallet!</button>
       </form>
@@ -95,6 +144,27 @@ function App() {
           <p>You've got {coins} coins!</p>
         </div>
       ) : null}
+      <div>
+        <p>Send some coins to someone else?</p>
+        <input
+          type="text"
+          name="recipient"
+          value={recipient}
+          onChange={event => onRecipientChange(event)}
+          placeholder="Recipient ID..."
+        />
+        <input
+          type="text"
+          name="transaction"
+          value={transaction}
+          onChange={event => onTransactionChange(event)}
+          placeholder="Recipient ID..."
+        />
+        <button onClick={event => onTransactionSubmit(event)}>
+          Send coins!
+        </button>
+      </div>
+      <div>{message}</div>
     </div>
   );
 }
